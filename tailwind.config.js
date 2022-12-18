05e1b37368dc0bc6db5311dac0ed4f9615a6980a
DS <ds@DSs-MacBook-Pro.lan>
2022-12-18 20:17:42 +0100

    Remove commented out code.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,4 @@
-// const colors = require('tailwindcss/colors')
-
 module.exports = {
-  // content: ['index.html', './src/**/*.{js,jsx,ts,tsx,vue,html}'],
   content: ["./src/**/*.{js,ts,html}"],
   theme: {
     extend: {},
